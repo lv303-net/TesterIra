32b916899f8277c0705728a3aed14dfabccbffe4
ira-halenok <ira-halenok@users.noreply.github.com>
2018-03-24 12:56:30 +0200

    Edited index.jsx by adding info about group

--- helloapp/index.jsx
+++ helloapp/index.jsx
@@ -1,7 +1,9 @@
 class Hello extends React.Component {
     render() {
-        return <h1>Hello, React</h1>
-        <div style="color:blue;">Made by Lv303-.NET</div>;
+        return (
+            <h1>Hello, React</h1>
+            <div style="color:blue;">Made by Lv303-.NET</div>
+        );
     }
 }
 ReactDOM.render(
