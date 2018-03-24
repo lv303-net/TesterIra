b70e48cc6120c228140e1d14d0ab6e7426364de2
ira-halenok <ira-halenok@users.noreply.github.com>
2018-03-24 12:51:07 +0200

    Update index.jsx

--- helloapp/index.jsx
+++ helloapp/index.jsx
@@ -1,6 +1,7 @@
 class Hello extends React.Component {
     render() {
-        return <h1>Hello, React</h1>;
+        return <h1>Hello, React</h1>
+        <div style="color:blue;">Made by Lv303-.NET</div>;
     }
 }
 ReactDOM.render(
