74d99aa30ea9a582b349ea4a0c7b9924531dd0b6
ira-halenok <ira-halenok@users.noreply.github.com>
2018-03-24 13:03:08 +0200

    Update index.jsx

--- helloapp/index.jsx
+++ helloapp/index.jsx
@@ -1,14 +1,8 @@
 class Hello extends React.Component {
     render() {
       return (
-        <div className="shopping-list">
-            <h1>Shopping List for {this.props.name}</h1>
-            <ul>
-                <li>Instagram</li>
-                <li>WhatsApp</li>
-                <li>Oculus</li>
-            </ul>
-        </div>
+        <h1>Hello World</h1>
+        <p>Made by Lv303-.NET</p>
       );
     }
 }
